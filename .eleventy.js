0c029250ef489d665a42d60779e789a613d043bb
Chris Rodriguez <Crodriguez4443@gmail.com>
2024-07-22 22:09:16 -0400

    added some CSS stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
-    eleventyConfig.addPassthroughCopy("scr/img");
+    eleventyConfig.addPassthroughCopy("src/img");
     return {
       dir: {
         input: "src",
